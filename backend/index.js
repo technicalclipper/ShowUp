aad14c962ea62c798aaecaf64b3231c816dccf96
technicalclipper <technicalclipper@gmail.com>
2025-07-04 23:05:02 +0200

    Init: Initialised db connection to manage bot and event state

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 const fs = require('fs');
 const path = require('path');
@@ -8,3 +9,18 @@ const token = process.env.token;
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', async (msg) => {
+    try {
+      // Log chat information to see group IDs
+      console.log('Chat Type:', msg.chat.type);
+      console.log('Chat ID:', msg.chat.id);
+      console.log('Chat Title:', msg.chat.title || 'Private Chat');
+      console.log('From User:', msg.from.first_name, msg.from.id);
+      
+      await bot.sendMessage(msg.chat.id, 'huh');
+    } catch (error) {
+      console.error('Error sending hi message:', error);
+    }
+  });
+  
